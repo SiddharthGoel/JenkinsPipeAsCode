50ac917ba3f194a364623c7871dcc136e691a18e
SiddharthGoel <Sidd456goel@gmail.com>
2020-05-24 17:45:31 +0530

    First Upload
    Async sln uploaded

---
+++ AdvancedAsyncSourceCode/WPFUserInterface/DemoMethods.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WPFUserInterface
+{
+    public static class DemoMethods
+    {
+        public static List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.amazon.com");
+            output.Add("https://www.facebook.com");
+            output.Add("https://www.twitter.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+            output.Add("https://en.wikipedia.org/wiki/.NET_Framework");
+
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadSync(System.Windows.Controls.ProgressBar dashboardProgress)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);                
+                dashboardProgress.Dispatcher.Invoke(() => dashboardProgress.Value = output.Count * 100 / websites.Count, DispatcherPriority.Background);
+            }
+
+            return output;
+        }
+        public static async Task<List<WebsiteDataModel>> RunDownloadAsync(IProgress<ProgressReportModel> progress)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = await DownloadWebsiteAsync(site);
+                output.Add(results);
+                report.PercentageComplete = output.Count * 100 / websites.Count;
+                report.SitesDownloaded = output;
+                progress.Report(report);
+            }
+
+            return output;
+        }
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync(IProgress<ProgressReportModel> progress)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = 6;
+            await Task.Run(() => {
+                Parallel.ForEach<string>(websites, parallelOptions, (site) =>
+                 {
+                     WebsiteDataModel results = DownloadWebsite(site);
+                     output.Add(results);
+                     report.PercentageComplete = output.Count * 100 / websites.Count;
+                     report.SitesDownloaded = output;
+                     progress.Report(report);
+                 });
+            });
+
+            return output;
+        }
+        private static async Task <WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+
+            return output;
+        }
+        private static WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+    }
+}
